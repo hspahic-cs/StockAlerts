1f3dd2b41db6848f1b342daa3d0cf581a001407e
Dom W <media.Dom0_o@protonmail.com>
2021-03-10 00:18:21 -0500

    epicness

--- gme.py
+++ gme.py
@@ -1,15 +1,6 @@
 #This python script is designed to display a live feed  of Gamestop's price
 #Dominic Wojewodka
 #03/09/2021
-import bs4
-import requests
-import datetime as dt
-import pytz
-import time
-import smtplib
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-
 from Stock_Alert import Stock 
     #List of functions in Stock
     #__init__(self,ticker)
