0bf875f7adb66c2755d6bc7e4e638aaa8fabc67a
Dom W <media.Dom0_o@protonmail.com>
2021-03-09 22:46:58 -0500

    spelling, to the moon

--- gme.py
+++ gme.py
@@ -1,4 +1,4 @@
-#This python script is designed to display a visual queue of Gamestocks Price "GME"
+#This python script is designed to display a live feed  of Gamestop's price
 #Dominic Wojewodka
 #03/09/2021
 
